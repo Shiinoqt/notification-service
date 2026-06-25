f53717c05d21146b77e80522b95fc3ffa264db32
Shiinoqt <damienrebong@outlook.com>
2026-06-25 11:58:33 +0200

    fix: app.prop

--- src/main/java/com/its/notificationservice/config/AwsS3ClientConfig.java
+++ src/main/java/com/its/notificationservice/config/AwsS3ClientConfig.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.time.Duration;
 
@@ -32,6 +33,18 @@ public class AwsS3ClientConfig {
                         )
                 )
                 .overrideConfiguration(b -> b.apiCallTimeout(Duration.ofMinutes(2)))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(awsKey, awsSecret)
+                        )
+                )
                 .build();
     }
 }
